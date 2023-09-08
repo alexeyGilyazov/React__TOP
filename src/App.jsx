ad3cfd1bd58e320b9fb83de8ba3e0da9cf3dfc44
Alexey Gi <alexeygilyazov43@gmail.com>
2023-09-08 22:01:46 +0300

    added 21 lesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import "../src/style/index.css";
 import Lesson21 from "./UDEMY/Lesson21/Lesson21";
 
+
 function App() {
+
+  const costs = [
+    {
+        date: new Date().toLocaleDateString(),
+        good: 'Macbook M2 ',
+        price: `1500`,
+    },
+    {
+        date: new Date().toLocaleDateString(),
+        good: 'Iphone 14 pro Max',
+        price: `999`,
+    },
+    {
+        date: new Date().toLocaleDateString(),
+        good: 'AirPods',
+        price: `400`,
+    }
+
+]
+
   return (
     <div className="App">
-      <Lesson21 />     
+      <Lesson21 date={costs[0].date} good={costs[0].good} price={costs[0].price} />
+      <Lesson21 date={costs[1].date} good={costs[1].good} price={costs[1].price} />
+      <Lesson21 date={costs[2].date} good={costs[2].good} price={costs[2].price} />
     </div>
   );
 }
--- src/UDEMY/Lesson21/Lesson21.jsx
+++ src/UDEMY/Lesson21/Lesson21.jsx
@@ -2,18 +2,15 @@
 import React from 'react'
 import './style.css'
 
-export default function Lesson21() {
-  
-  const currentDate = new Date().toLocaleDateString()
-  const good = 'Macbook'
+export default function Lesson21({date,good,price}) {
 
   return (
     <>
       <div className="cost-item">
-        <div className="date">{currentDate}</div>
+        <div className="date">{date}</div>
         <div className="cost-item__desc">
           <h2>{good}</h2>
-          <div className="cost-item__price">999$</div>
+          <div className="cost-item__price">{price}</div>
         </div>
       </div>
     </>
