0c6d745ce38efb0eea8287b40201917ed56db4c7
Alexey Gi <alexeygilyazov43@gmail.com>
2023-09-09 22:00:50 +0300

    added new lesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
 import "../src/style/index.css";
-import Lesson21 from "./UDEMY/Lesson21/Lesson21";
-
+import React from 'react';
+import Cost from "./UDEMY/Lesson23/Cost";
+import Lesson32 from "./UDEMY/Lesson32/Lesson23";
 
 function App() {
 
   const costs = [
     {
-        date: new Date().toLocaleDateString(),
-        good: 'Macbook M2 ',
-        price: `1500`,
+      date: new Date().toLocaleDateString(),
+      good: 'Macbook M2',
+      price: '1500',
     },
     {
-        date: new Date().toLocaleDateString(),
-        good: 'Iphone 14 pro Max',
-        price: `999`,
+      date: new Date().toLocaleDateString(),
+      good: 'Iphone 14 pro Max',
+      price: '999',
     },
     {
-        date: new Date().toLocaleDateString(),
-        good: 'AirPods',
-        price: `400`,
-    }
-
-]
+      date: new Date().toLocaleDateString(),
+      good: 'AirPods',
+      price: '400',
+    },
+  ];
 
   return (
     <div className="App">
-      <Lesson21 date={costs[0].date} good={costs[0].good} price={costs[0].price} />
-      <Lesson21 date={costs[1].date} good={costs[1].good} price={costs[1].price} />
-      <Lesson21 date={costs[2].date} good={costs[2].good} price={costs[2].price} />
+      <Cost>
+        {costs.map((cost, index) => (
+          <Lesson32 key={index} date={cost} />
+        ))}
+      </Cost>
     </div>
   );
 }
---
+++ src/UDEMY/Lesson32/Cost.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './style.css'
+
+export default function Cost(props) {
+    return (
+        <div className='card'>{props.children}</div>
+    )
+}
